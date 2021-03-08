a3c00097507f5728cf318ba3c17046fe06e886d6
eshankt <eshankt@outlook.com>
2021-03-08 17:49:32 +0530

    react js file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello From CloudThat!</h1>
+        <img src = "Cloudthat.png" className = "image"></img>
+    </div>
+  );
+}
+
+export default App;
